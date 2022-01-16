804ac70377d0c305e15b57a411f50e1bdc30796f
Ozioma <oziomaokoroafor@gmail.com>
2022-01-16 10:54:42 +0100

    created demo controller

---
+++ src/main/java/africa/ozi/files/controller/DemoController.java
@@ -0,0 +1,13 @@
+package africa.ozi.files.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("welcome")
+    public String welcomeFirstTimer(){
+        return "Welcome to my rest controller";
+    }
+}
---
+++ src/main/java/africa/ozi/files/controller/DemoControllerMain.java
@@ -0,0 +1,13 @@
+package africa.ozi.files.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DemoControllerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoControllerMain.class, args);
+
+    }
+}
